1a879284b2d1fd40580440e59c98570afa6664b5
CodeByGab <gabrielsoares1602@gmail.com>
2026-04-14 23:27:03 -0300

    more stats in pdf :3

--- pdf_utils/render_pdf.py
+++ pdf_utils/render_pdf.py
@@ -23,25 +23,27 @@ def generate_pdf(chac):
             "WISmod": math.calc_stt(chac["stats"]["wis"]),
             "CHA": chac["stats"]["cha"],
             "CHamod": math.calc_stt(chac["stats"]["cha"]),
-        }
+            "ProfBonus": chac["proficiency_bonus"],
+            "Passive": chac["pass_perception"],
+            "HPMax": chac["hp"],
+            "HPCurrent": chac["hp"],
+            "AC": chac["ac"],
+            "Initiative": chac["initiative"],
+            "Speed": chac["speed"],
+            "HD": chac["hit_die"],
+       }
 
     filled = PdfWrapper(PDF_NAME, need_appearances=True).fill(character_infos)
 
     filled.write(f"{OUTPUT_PRE}{chac['name']} {chac['class']} {chac['level']}.pdf")
 
-    #     "hp": max_hp,
     #     "hit_die": hit_die,
-    #     "proficiency_bonus": proficiency_bonus,
     #     "stats": ability_scores,
     #     "saves": {
     #         st1: save1,
     #         st2: save2
     #     },
     #     "skills": prof_skills,
-    #     "speed": race_speed,
-    #     "initiative": initiative,
-    #     "ac": ac,
-    #     "pass_perception": pass_perception,
     #     "proficiencies": proficiencies,
     #     "inventory": inventory,
     #     "features": features,
